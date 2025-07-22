480ad3a595da32a4ef8c8936758ea5c320c62d20
Shem <oduor2shem@gmail.com>
2025-07-22 10:27:51 +0300

    Imports

---
+++ Chess.py
@@ -0,0 +1,11 @@
+import pygame, chess, sys, os
+from math import inf
+
+
+SQ = 60                  
+WIDTH = HEIGHT = SQ * 8
+FPS = 30
+WHITE = (235, 235, 210)
+BLACK = (119, 154, 88)
+SELECT = (186, 202, 68)
+CHECK = (207,  95,  95)
